9f25aff41dbdffa17890aa3a242b3e6b4b2b0f00
judsonpaiva <judson.paiva@hotmail.com>
2022-03-31 20:44:07 +0100

    first commit

---
+++ app/src/main/java/com/example/dados_pessoais_versao_ii/Pessoa.java
@@ -0,0 +1,18 @@
+package com.example.dados_pessoais_versao_ii;
+
+public class Pessoa {
+
+    private String nome;
+    private String morada;
+    private String telemovel;
+    private String sexo;
+
+    public Pessoa(String nome, String morada, String telemovel, String sexo) {
+        this.nome = nome;
+        this.morada = morada;
+        this.telemovel = telemovel;
+        this.sexo = sexo;
+    }
+
+
+}
