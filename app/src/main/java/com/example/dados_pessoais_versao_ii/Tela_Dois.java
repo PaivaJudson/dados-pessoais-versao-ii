1467a84066b98f41cffd1762b6c5d1fa11985a38
judsonpaiva <judson.paiva@hotmail.com>
2022-03-31 21:23:44 +0100

    classe Pessoa

---
+++ app/src/main/java/com/example/dados_pessoais_versao_ii/Tela_Dois.java
@@ -0,0 +1,14 @@
+package com.example.dados_pessoais_versao_ii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Dois extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_dois);
+    }
+}
